4e1d59e0e2016108af6b107753391bcea3dc98fe
Andis Redmans <andis.redmans@sap.com>
2025-02-10 08:47:59 +0200

    correction
    correction

--- src/extension.ts
+++ src/extension.ts
@@ -4,31 +4,6 @@ import * as vscode from "vscode";
 import { copyOrMoveFile, selectFolder } from "./files";
 import { join } from "path";
 
-/**
- * Get watch pattern string for 'vscode.createFileSystemWatcher'.
- * Method returns pattern, which works on desktop VSCode and SBAS environment.
- * If you changing code of that method, then please check do your changes work on SBAS environment.
- * @param root - root path.
- * @param {...string} pathParts - Paths parts which should be concated/joined together and with root.
- * @return A path string.
- */
-const createWatchPatternString = (
-  rootPath: string,
-  ...pathParts: string[]
-): string => {
-  let path: string;
-  if (rootPath) {
-    path = join(rootPath, ...pathParts);
-  } else {
-    path = join(...pathParts);
-  }
-  const watchPrefix = ["darwin", "win32"].includes(process.platform)
-    ? "**"
-    : "";
-  path = watchPrefix + vscode.Uri.file(path).path;
-  return path;
-};
-
 export function activate(context: vscode.ExtensionContext) {
   console.log('Test says "Hello"');
 
@@ -51,11 +26,11 @@ export function activate(context: vscode.ExtensionContext) {
       if (selectedFolder) {
         const pattern = join(selectedFolder, "**", "*.json");
         // Watcher directly to file
-        const watch = vscode.workspace.createFileSystemWatcher(
-          createWatchPatternString(pattern)
-        );
+        const watch = vscode.workspace.createFileSystemWatcher(pattern);
         watch.onDidChange((uri: vscode.Uri) => {
-          vscode.window.showInformationMessage("Change watcher is triggered");
+          vscode.window.showInformationMessage(
+            "Change watcher for '*.json' is triggered"
+          );
         });
         watch.onDidCreate((uri: vscode.Uri) => {
           vscode.window.showInformationMessage("Create watcher is triggered");
@@ -63,11 +38,11 @@ export function activate(context: vscode.ExtensionContext) {
 
         const pattern2 = join(selectedFolder, "**");
         // Watcher directly to file
-        const watch2 = vscode.workspace.createFileSystemWatcher(
-          createWatchPatternString(pattern2)
-        );
+        const watch2 = vscode.workspace.createFileSystemWatcher(pattern2);
         watch2.onDidChange((uri: vscode.Uri) => {
-          vscode.window.showInformationMessage("Change watcher 2 is triggered");
+          vscode.window.showInformationMessage(
+            "Change watcher for '*' is triggered"
+          );
         });
         watch2.onDidCreate((uri: vscode.Uri) => {
           vscode.window.showInformationMessage("Create watcher 2 is triggered");
